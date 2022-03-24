b31bc7c1b13faf339cca411f4c10fc4517b45aa2
Qaiser Shakoor <qaiser@nclouds.com>
2022-03-24 17:33:26 +0500

    pushed

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
-    install_requires=['peppercorn','PyMySQL','SQLAlchemy'],  # Optional
+    install_requires=['peppercorn','PyMySQL','SQLAlchemy','boto3'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"

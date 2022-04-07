7a600870aebe9260e0de4158c25f347723292708
Qaiser Shakoor <link2qaiser@gmail.com>
2022-04-07 17:10:52 +0500

    update the config

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='SQL ORM Code',  # Optional
+    description='Ncall global functions',  
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
@@ -78,7 +78,7 @@ setup(
 
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='link2qaiser@gmail.com',  # Optional
+    author_email='qaiser@nclouds.com',  # Optional
 
     # Classifiers help users find your project by categorizing it.
     #
